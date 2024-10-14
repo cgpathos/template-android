2c639165df1a674bf1c6ce1a51c046ca9b9f3859
pathos <neo@pathos.today>
2024-10-14 18:36:48 +0900

    [2024.10.00] - 각종 버전 최신화 - edge to edge 적용

--- app/src/main/java/android/template/MainActivity.kt
+++ app/src/main/java/android/template/MainActivity.kt
@@ -1,25 +1,43 @@
 package android.template
 
 import android.os.Bundle
+import android.template.ui.theme.TemplateAndroidTheme
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import android.template.ui.theme.TemplateAndroidTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
         setContent {
             TemplateAndroidTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Scaffold { paddingValues ->
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(color = MaterialTheme.colorScheme.surface)
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .padding(paddingValues)
+                                .fillMaxSize()
+                                .background(color = MaterialTheme.colorScheme.surfaceVariant)
+                        ) {
+                            Greeting("Android")
+                        }
+                    }
                 }
             }
         }
