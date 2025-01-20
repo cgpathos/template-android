d76543c3510f257e31d1b201582c386469e488e3
pathos <neo@pathos.today>
2025-01-20 23:10:00 +0900

    bump up dependencies (#24)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,17 +1,20 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.jetbrains.kotlin.serialization)
     alias(libs.plugins.jetbrains.compose.compiler)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.secrets)
 }
 
 android {
     namespace = "android.template"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "android.template"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -23,26 +26,43 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        jvmToolchain(17)
+
+        // fixme : 언젠가 beta 풀리면 설정 활성화 하기
+//        sourceSets.all {
+//            languageSettings.enableLanguageFeature("ExplicitBackingFields")
+//        }
     }
     buildFeatures {
         compose = true
     }
-    composeOptions {
-    }
     packaging {
         resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "/META-INF/{AL2.0,LGPL2.1,LICENSE*.md}"
         }
     }
+}
+
+composeCompiler {
+    reportsDestination = layout.buildDirectory.dir("compose_compiler")
+    metricsDestination = layout.buildDirectory.dir("compose_compiler")
+    stabilityConfigurationFiles =
+        listOf(rootProject.layout.projectDirectory.file("stability_config.conf"))
+}
+
+secrets {
+    defaultPropertiesFileName = "secrets.defaults.properties"
 }
 
 dependencies {
@@ -53,7 +73,7 @@ dependencies {
 
     implementation(libs.core.ktx)
 
-    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.activity.compose)
 
     // compose
@@ -64,7 +84,16 @@ dependencies {
     androidTestImplementation(libs.bundles.compose.android.test)
     debugImplementation(libs.bundles.compose.ui.test)
 
+    // logging
+    implementation(libs.timber)
+
+    // test
     testImplementation(libs.junit)
+    testImplementation(libs.mockk.android)
+    testImplementation(libs.mockk.agent)
+    testImplementation(libs.robolectric)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.mockk.android)
+    androidTestImplementation(libs.mockk.agent)
 }
