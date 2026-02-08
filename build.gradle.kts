593486d1a414d318366ca9c5b9fb962e85f755fd
pathos <neo@pathos.today>
2026-02-09 00:18:52 +0900

    오랜만에 라이브러리 업데이트

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,11 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.jetbrains.kotlin.serialization) apply false
-    alias(libs.plugins.jetbrains.compose.compiler) apply false
+    alias(libs.plugins.jetbrains.compose) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.hilt.android) apply false
+    alias(libs.plugins.androidx.room) apply false
     alias(libs.plugins.secrets) apply false
+    alias(libs.plugins.gms.oss.licenses.plugin) apply false
 }
